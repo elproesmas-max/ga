413087e4f709197569bfbe61b616f1b2db079997
elproesmas-max <elproesmas@gmail.com>
2026-03-24 10:45:55 -0500

    Delete proyecto para brujo directory

--- proyecto para brujo/script.js
+++
@@ -1,7 +0,0 @@
-function iniciar(){
-  document.querySelector(".btn").style.display="none";
-  document.getElementById("contenido").style.display="block";
-
-  let audio = document.getElementById("audio");
-  audio.play().catch(()=>{});
-}
